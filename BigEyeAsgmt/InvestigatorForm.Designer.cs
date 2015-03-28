bfe9e5a51bf2ee8a5f058a53fe5f83f9f5293af7
VictorFeng <VictorF@foxmail.com>
2015-03-28 14:05:22 +1300

    Developed the investigator module

--- BigEyeAsgmt/InvestigatorForm.Designer.cs
+++ BigEyeAsgmt/InvestigatorForm.Designer.cs
@@ -39,7 +39,7 @@
             this.btnPrevious = new System.Windows.Forms.Button();
             this.btnNext = new System.Windows.Forms.Button();
             this.btnAddInvestigator = new System.Windows.Forms.Button();
-            this.btnUpdateInvestigator = new System.Windows.Forms.Button();
+            this.btnModifyInvestigator = new System.Windows.Forms.Button();
             this.btnDeleteInvestigator = new System.Windows.Forms.Button();
             this.btnReturn = new System.Windows.Forms.Button();
             this.txtLastName = new System.Windows.Forms.TextBox();
@@ -47,6 +47,24 @@
             this.txtHourlyRate = new System.Windows.Forms.TextBox();
             this.txtCellPhone = new System.Windows.Forms.TextBox();
             this.txtLicensed = new System.Windows.Forms.TextBox();
+            this.pnlInvestigator = new System.Windows.Forms.Panel();
+            this.txtPnlLicensed = new System.Windows.Forms.TextBox();
+            this.ckLicensedFalse = new System.Windows.Forms.CheckBox();
+            this.ckLicensedTrue = new System.Windows.Forms.CheckBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSaveInvestigator = new System.Windows.Forms.Button();
+            this.txtPnlCellPhone = new System.Windows.Forms.TextBox();
+            this.txtPnlHourlyRate = new System.Windows.Forms.TextBox();
+            this.txtPnlFirstName = new System.Windows.Forms.TextBox();
+            this.txtPnlLastName = new System.Windows.Forms.TextBox();
+            this.lblPnlLicensed = new System.Windows.Forms.Label();
+            this.lblPnlCellPhone = new System.Windows.Forms.Label();
+            this.lblPnlHourlyRate = new System.Windows.Forms.Label();
+            this.lblPnlFirstName = new System.Windows.Forms.Label();
+            this.lblPnlLastName = new System.Windows.Forms.Label();
+            this.lblPnlInvestigatorID = new System.Windows.Forms.Label();
+            this.lblPnlInvestigatorNO = new System.Windows.Forms.Label();
+            this.pnlInvestigator.SuspendLayout();
             this.SuspendLayout();
             // 
             // lstInvestigators
@@ -149,15 +167,17 @@
             this.btnAddInvestigator.TabIndex = 10;
             this.btnAddInvestigator.Text = "Add Investigator";
             this.btnAddInvestigator.UseVisualStyleBackColor = true;
+            this.btnAddInvestigator.Click += new System.EventHandler(this.btnAddInvestigator_Click);
             // 
-            // btnUpdateInvestigator
+            // btnModifyInvestigator
             // 
-            this.btnUpdateInvestigator.Location = new System.Drawing.Point(515, 342);
-            this.btnUpdateInvestigator.Name = "btnUpdateInvestigator";
-            this.btnUpdateInvestigator.Size = new System.Drawing.Size(165, 45);
-            this.btnUpdateInvestigator.TabIndex = 11;
-            this.btnUpdateInvestigator.Text = "Update Investigator";
-            this.btnUpdateInvestigator.UseVisualStyleBackColor = true;
+            this.btnModifyInvestigator.Location = new System.Drawing.Point(515, 342);
+            this.btnModifyInvestigator.Name = "btnModifyInvestigator";
+            this.btnModifyInvestigator.Size = new System.Drawing.Size(165, 45);
+            this.btnModifyInvestigator.TabIndex = 11;
+            this.btnModifyInvestigator.Text = "Modify Investigator";
+            this.btnModifyInvestigator.UseVisualStyleBackColor = true;
+            this.btnModifyInvestigator.Click += new System.EventHandler(this.btnModifyInvestigator_Click);
             // 
             // btnDeleteInvestigator
             // 
@@ -167,6 +187,7 @@
             this.btnDeleteInvestigator.TabIndex = 12;
             this.btnDeleteInvestigator.Text = "Delete Investigator";
             this.btnDeleteInvestigator.UseVisualStyleBackColor = true;
+            this.btnDeleteInvestigator.Click += new System.EventHandler(this.btnDeleteInvestigator_Click);
             // 
             // btnReturn
             // 
@@ -213,11 +234,176 @@
             this.txtLicensed.Size = new System.Drawing.Size(100, 26);
             this.txtLicensed.TabIndex = 18;
             // 
+            // pnlInvestigator
+            // 
+            this.pnlInvestigator.Controls.Add(this.txtPnlLicensed);
+            this.pnlInvestigator.Controls.Add(this.ckLicensedFalse);
+            this.pnlInvestigator.Controls.Add(this.ckLicensedTrue);
+            this.pnlInvestigator.Controls.Add(this.btnCancel);
+            this.pnlInvestigator.Controls.Add(this.btnSaveInvestigator);
+            this.pnlInvestigator.Controls.Add(this.txtPnlCellPhone);
+            this.pnlInvestigator.Controls.Add(this.txtPnlHourlyRate);
+            this.pnlInvestigator.Controls.Add(this.txtPnlFirstName);
+            this.pnlInvestigator.Controls.Add(this.txtPnlLastName);
+            this.pnlInvestigator.Controls.Add(this.lblPnlLicensed);
+            this.pnlInvestigator.Controls.Add(this.lblPnlCellPhone);
+            this.pnlInvestigator.Controls.Add(this.lblPnlHourlyRate);
+            this.pnlInvestigator.Controls.Add(this.lblPnlFirstName);
+            this.pnlInvestigator.Controls.Add(this.lblPnlLastName);
+            this.pnlInvestigator.Controls.Add(this.lblPnlInvestigatorID);
+            this.pnlInvestigator.Controls.Add(this.lblPnlInvestigatorNO);
+            this.pnlInvestigator.Location = new System.Drawing.Point(232, 12);
+            this.pnlInvestigator.Name = "pnlInvestigator";
+            this.pnlInvestigator.Size = new System.Drawing.Size(389, 324);
+            this.pnlInvestigator.TabIndex = 19;
+            this.pnlInvestigator.Visible = false;
+            // 
+            // txtPnlLicensed
+            // 
+            this.txtPnlLicensed.Location = new System.Drawing.Point(163, 230);
+            this.txtPnlLicensed.Name = "txtPnlLicensed";
+            this.txtPnlLicensed.Size = new System.Drawing.Size(72, 26);
+            this.txtPnlLicensed.TabIndex = 16;
+            // 
+            // ckLicensedFalse
+            // 
+            this.ckLicensedFalse.AutoSize = true;
+            this.ckLicensedFalse.Location = new System.Drawing.Point(236, 232);
+            this.ckLicensedFalse.Name = "ckLicensedFalse";
+            this.ckLicensedFalse.Size = new System.Drawing.Size(74, 24);
+            this.ckLicensedFalse.TabIndex = 15;
+            this.ckLicensedFalse.Text = "False";
+            this.ckLicensedFalse.UseVisualStyleBackColor = true;
+            this.ckLicensedFalse.CheckStateChanged += new System.EventHandler(this.ckLicensedFalse_CheckStateChanged);
+            // 
+            // ckLicensedTrue
+            // 
+            this.ckLicensedTrue.AutoSize = true;
+            this.ckLicensedTrue.Location = new System.Drawing.Point(163, 232);
+            this.ckLicensedTrue.Name = "ckLicensedTrue";
+            this.ckLicensedTrue.Size = new System.Drawing.Size(67, 24);
+            this.ckLicensedTrue.TabIndex = 14;
+            this.ckLicensedTrue.Text = "True";
+            this.ckLicensedTrue.UseVisualStyleBackColor = true;
+            this.ckLicensedTrue.CheckStateChanged += new System.EventHandler(this.ckLicensedTrue_CheckStateChanged);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(223, 270);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(110, 45);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSaveInvestigator
+            // 
+            this.btnSaveInvestigator.Location = new System.Drawing.Point(44, 270);
+            this.btnSaveInvestigator.Name = "btnSaveInvestigator";
+            this.btnSaveInvestigator.Size = new System.Drawing.Size(163, 45);
+            this.btnSaveInvestigator.TabIndex = 12;
+            this.btnSaveInvestigator.Text = "Save Investigator";
+            this.btnSaveInvestigator.UseVisualStyleBackColor = true;
+            this.btnSaveInvestigator.Click += new System.EventHandler(this.btnSaveInvestigator_Click);
+            // 
+            // txtPnlCellPhone
+            // 
+            this.txtPnlCellPhone.Location = new System.Drawing.Point(163, 186);
+            this.txtPnlCellPhone.Name = "txtPnlCellPhone";
+            this.txtPnlCellPhone.Size = new System.Drawing.Size(125, 26);
+            this.txtPnlCellPhone.TabIndex = 10;
+            // 
+            // txtPnlHourlyRate
+            // 
+            this.txtPnlHourlyRate.Location = new System.Drawing.Point(163, 142);
+            this.txtPnlHourlyRate.Name = "txtPnlHourlyRate";
+            this.txtPnlHourlyRate.Size = new System.Drawing.Size(100, 26);
+            this.txtPnlHourlyRate.TabIndex = 9;
+            // 
+            // txtPnlFirstName
+            // 
+            this.txtPnlFirstName.Location = new System.Drawing.Point(163, 98);
+            this.txtPnlFirstName.Name = "txtPnlFirstName";
+            this.txtPnlFirstName.Size = new System.Drawing.Size(100, 26);
+            this.txtPnlFirstName.TabIndex = 8;
+            // 
+            // txtPnlLastName
+            // 
+            this.txtPnlLastName.Location = new System.Drawing.Point(163, 54);
+            this.txtPnlLastName.Name = "txtPnlLastName";
+            this.txtPnlLastName.Size = new System.Drawing.Size(100, 26);
+            this.txtPnlLastName.TabIndex = 7;
+            // 
+            // lblPnlLicensed
+            // 
+            this.lblPnlLicensed.AutoSize = true;
+            this.lblPnlLicensed.Location = new System.Drawing.Point(80, 233);
+            this.lblPnlLicensed.Name = "lblPnlLicensed";
+            this.lblPnlLicensed.Size = new System.Drawing.Size(77, 20);
+            this.lblPnlLicensed.TabIndex = 6;
+            this.lblPnlLicensed.Text = "Licensed:";
+            // 
+            // lblPnlCellPhone
+            // 
+            this.lblPnlCellPhone.AutoSize = true;
+            this.lblPnlCellPhone.Location = new System.Drawing.Point(68, 189);
+            this.lblPnlCellPhone.Name = "lblPnlCellPhone";
+            this.lblPnlCellPhone.Size = new System.Drawing.Size(89, 20);
+            this.lblPnlCellPhone.TabIndex = 5;
+            this.lblPnlCellPhone.Text = "Cell Phone:";
+            // 
+            // lblPnlHourlyRate
+            // 
+            this.lblPnlHourlyRate.AutoSize = true;
+            this.lblPnlHourlyRate.Location = new System.Drawing.Point(60, 145);
+            this.lblPnlHourlyRate.Name = "lblPnlHourlyRate";
+            this.lblPnlHourlyRate.Size = new System.Drawing.Size(97, 20);
+            this.lblPnlHourlyRate.TabIndex = 4;
+            this.lblPnlHourlyRate.Text = "Hourly Rate:";
+            // 
+            // lblPnlFirstName
+            // 
+            this.lblPnlFirstName.AutoSize = true;
+            this.lblPnlFirstName.Location = new System.Drawing.Point(67, 101);
+            this.lblPnlFirstName.Name = "lblPnlFirstName";
+            this.lblPnlFirstName.Size = new System.Drawing.Size(90, 20);
+            this.lblPnlFirstName.TabIndex = 3;
+            this.lblPnlFirstName.Text = "First Name:";
+            // 
+            // lblPnlLastName
+            // 
+            this.lblPnlLastName.AutoSize = true;
+            this.lblPnlLastName.Location = new System.Drawing.Point(67, 57);
+            this.lblPnlLastName.Name = "lblPnlLastName";
+            this.lblPnlLastName.Size = new System.Drawing.Size(90, 20);
+            this.lblPnlLastName.TabIndex = 2;
+            this.lblPnlLastName.Text = "Last Name:";
+            // 
+            // lblPnlInvestigatorID
+            // 
+            this.lblPnlInvestigatorID.AutoSize = true;
+            this.lblPnlInvestigatorID.Location = new System.Drawing.Point(163, 13);
+            this.lblPnlInvestigatorID.Name = "lblPnlInvestigatorID";
+            this.lblPnlInvestigatorID.Size = new System.Drawing.Size(90, 20);
+            this.lblPnlInvestigatorID.TabIndex = 1;
+            this.lblPnlInvestigatorID.Text = "investigator";
+            // 
+            // lblPnlInvestigatorNO
+            // 
+            this.lblPnlInvestigatorNO.AutoSize = true;
+            this.lblPnlInvestigatorNO.Location = new System.Drawing.Point(40, 13);
+            this.lblPnlInvestigatorNO.Name = "lblPnlInvestigatorNO";
+            this.lblPnlInvestigatorNO.Size = new System.Drawing.Size(117, 20);
+            this.lblPnlInvestigatorNO.TabIndex = 0;
+            this.lblPnlInvestigatorNO.Text = "Investigator ID:";
+            // 
             // InvestigatorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(880, 483);
+            this.Controls.Add(this.pnlInvestigator);
             this.Controls.Add(this.txtLicensed);
             this.Controls.Add(this.txtCellPhone);
             this.Controls.Add(this.txtHourlyRate);
@@ -225,7 +411,7 @@
             this.Controls.Add(this.txtLastName);
             this.Controls.Add(this.btnReturn);
             this.Controls.Add(this.btnDeleteInvestigator);
-            this.Controls.Add(this.btnUpdateInvestigator);
+            this.Controls.Add(this.btnModifyInvestigator);
             this.Controls.Add(this.btnAddInvestigator);
             this.Controls.Add(this.btnNext);
             this.Controls.Add(this.btnPrevious);
@@ -239,6 +425,8 @@
             this.Controls.Add(this.lstInvestigators);
             this.Name = "InvestigatorForm";
             this.Text = "InvestigatorForm";
+            this.pnlInvestigator.ResumeLayout(false);
+            this.pnlInvestigator.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -257,7 +445,7 @@
         private System.Windows.Forms.Button btnPrevious;
         private System.Windows.Forms.Button btnNext;
         private System.Windows.Forms.Button btnAddInvestigator;
-        private System.Windows.Forms.Button btnUpdateInvestigator;
+        private System.Windows.Forms.Button btnModifyInvestigator;
         private System.Windows.Forms.Button btnDeleteInvestigator;
         private System.Windows.Forms.Button btnReturn;
         private System.Windows.Forms.TextBox txtLastName;
@@ -265,5 +453,22 @@
         private System.Windows.Forms.TextBox txtHourlyRate;
         private System.Windows.Forms.TextBox txtCellPhone;
         private System.Windows.Forms.TextBox txtLicensed;
+        private System.Windows.Forms.Panel pnlInvestigator;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSaveInvestigator;
+        private System.Windows.Forms.TextBox txtPnlCellPhone;
+        private System.Windows.Forms.TextBox txtPnlHourlyRate;
+        private System.Windows.Forms.TextBox txtPnlFirstName;
+        private System.Windows.Forms.TextBox txtPnlLastName;
+        private System.Windows.Forms.Label lblPnlLicensed;
+        private System.Windows.Forms.Label lblPnlCellPhone;
+        private System.Windows.Forms.Label lblPnlHourlyRate;
+        private System.Windows.Forms.Label lblPnlFirstName;
+        private System.Windows.Forms.Label lblPnlLastName;
+        private System.Windows.Forms.Label lblPnlInvestigatorID;
+        private System.Windows.Forms.Label lblPnlInvestigatorNO;
+        private System.Windows.Forms.CheckBox ckLicensedFalse;
+        private System.Windows.Forms.CheckBox ckLicensedTrue;
+        private System.Windows.Forms.TextBox txtPnlLicensed;
     }
 }
--- BigEyeAsgmt/InvestigatorForm.cs
+++ BigEyeAsgmt/InvestigatorForm.cs
@@ -72,6 +72,176 @@ namespace BigEyeAsgmt
             }
         }
 
+        private void btnAddInvestigator_Click(object sender, EventArgs e)
+        {
+            disableInvestigatorFormElements();
+            resetInvestigatorVal();
+            lblPnlInvestigatorID.Visible = false;
+            lblPnlInvestigatorNO.Visible = false;
+            ckLicensedTrue.Visible = false;
+            ckLicensedFalse.Visible = false;
 
+            btnSaveInvestigator.Text = "Save Investigator";
+
+            pnlInvestigator.Show();
+        }
+
+        private void btnModifyInvestigator_Click(object sender, EventArgs e)
+        {
+            disableInvestigatorFormElements();
+            resetInvestigatorVal();
+            lblPnlInvestigatorID.Visible = true;
+            lblPnlInvestigatorNO.Visible = true;
+            txtPnlLicensed.Visible = false;
+            ckLicensedFalse.Visible = true;
+            ckLicensedTrue.Visible = true;
+            btnSaveInvestigator.Text = "Update Investigator";
+
+            DataRow modifyInvestigator = DM.dtInvestigator.Rows[currencyManager.Position];
+            lblPnlInvestigatorID.Text = modifyInvestigator["InvestigatorID"].ToString();
+            txtPnlLastName.Text = modifyInvestigator["LastName"].ToString();
+            txtPnlFirstName.Text = modifyInvestigator["FirstName"].ToString();
+            txtPnlHourlyRate.Text = modifyInvestigator["HourlyRate"].ToString();
+            txtPnlCellPhone.Text = modifyInvestigator["CellPhone"].ToString();
+            string licensed = modifyInvestigator["Licensed"].ToString();
+            if (licensed.Equals("True"))
+            {
+                ckLicensedTrue.Checked = true;
+            }
+            else
+            {
+                ckLicensedFalse.Checked = true;
+            }
+
+            pnlInvestigator.Show();
+        }
+
+        private void btnSaveInvestigator_Click(object sender, EventArgs e)
+        {
+            if (txtPnlLastName.Text == "" || txtPnlFirstName.Text == "" || txtPnlHourlyRate.Text == ""
+                || txtPnlCellPhone.Text == "")
+            {
+                MessageBox.Show("You must enter a value for each of the text fields", "Error");
+            }
+            else
+            {
+                DataRow investigatorRow;
+                if (lblPnlInvestigatorID.Text == null || lblPnlInvestigatorID.Text.Equals(""))
+                {
+                    investigatorRow = DM.dtInvestigator.NewRow();
+                    investigatorRow["LastName"] = txtPnlLastName.Text;
+                    investigatorRow["FirstName"] = txtPnlFirstName.Text;
+                    investigatorRow["HourlyRate"] = txtPnlHourlyRate.Text;
+                    investigatorRow["CellPhone"] = txtPnlCellPhone.Text;
+                    investigatorRow["Licensed"] = txtPnlLicensed.Text == ""?"False":"True";
+
+                    DM.dtInvestigator.Rows.Add(investigatorRow);
+                    DM.updateInvestigator();
+
+                    enableInvestigatorFormElements();
+                    pnlInvestigator.Hide();
+
+                    MessageBox.Show("Investigator added successfully", "Success");
+                }
+                else
+                {
+                    investigatorRow = DM.dtInvestigator.Rows[currencyManager.Position];
+                    investigatorRow["LastName"] = txtPnlLastName.Text;
+                    investigatorRow["FirstName"] = txtPnlFirstName.Text;
+                    investigatorRow["HourlyRate"] = txtPnlHourlyRate.Text;
+                    investigatorRow["CellPhone"] = txtPnlCellPhone.Text;
+                    string licensed = ckLicensedTrue.Checked ? "True" : "False";
+                    investigatorRow["Licensed"] = licensed;
+
+                    currencyManager.EndCurrentEdit();
+                    DM.updateInvestigator();
+
+                    enableInvestigatorFormElements();
+                    pnlInvestigator.Hide();
+
+                    MessageBox.Show("Investigator updated successfully", "Success");
+                }
+            }
+        }
+
+        private void btnDeleteInvestigator_Click(object sender, EventArgs e)
+        {
+            DataRow deleteInvestigatorRow = DM.dtInvestigator.Rows[currencyManager.Position];
+            DataRow[] asgmtRow = DM.dtAssignment.Select("InvestigatorID = " + lblInvestigatorID.Text);
+            DataRow[] equipmentRow = DM.dtEquipment.Select("InvestigatorID = " + lblInvestigatorID.Text);
+            if (asgmtRow.Length != 0)
+            {
+                MessageBox.Show("You may only delete Investigators who are not assigned to cases", "Error");
+            }
+            else if (equipmentRow.Length != 0)
+            {
+                MessageBox.Show("You may only delete Investigators who are not allocated equipment", "Error");
+            }
+            else
+            {
+                if (MessageBox.Show("Are you sure you want to delete this records?", "Warning",
+                                    MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    deleteInvestigatorRow.Delete();
+                    DM.updateInvestigator();
+                }
+
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            enableInvestigatorFormElements();
+
+            pnlInvestigator.Hide();
+        }
+
+        private void enableInvestigatorFormElements()
+        {
+            lstInvestigators.Visible = true;
+            btnPrevious.Enabled = true;
+            btnNext.Enabled = true;
+            btnAddInvestigator.Enabled = true;
+            btnModifyInvestigator.Enabled = true;
+            btnDeleteInvestigator.Enabled = true;
+            btnReturn.Enabled = true;
+        }
+
+        private void disableInvestigatorFormElements()
+        {
+            lstInvestigators.Visible = false;
+            btnPrevious.Enabled = false;
+            btnNext.Enabled = false;
+            btnAddInvestigator.Enabled = false;
+            btnModifyInvestigator.Enabled = false;
+            btnDeleteInvestigator.Enabled = false;
+            btnReturn.Enabled = false;
+        }
+
+        private void resetInvestigatorVal()
+        {
+            lblPnlInvestigatorID.Text = null;
+            txtPnlLastName.Text = "";
+            txtPnlFirstName.Text = "";
+            txtPnlHourlyRate.Text = "";
+            txtPnlCellPhone.Text = "";
+            txtPnlLicensed.Text = "";
+        }
+
+        private void ckLicensedTrue_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (ckLicensedTrue.Checked)
+            {
+                ckLicensedFalse.Checked = false;
+            }
+        }
+
+        private void ckLicensedFalse_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (ckLicensedFalse.Checked)
+            {
+                ckLicensedTrue.Checked = false;
+            }
+        }
     }
 }
